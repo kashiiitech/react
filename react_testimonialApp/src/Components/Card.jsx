cfd05b941c5d19a52e298777f18e50ba1c9e65ad
Kashif Ali <kashiiitech@gmail.com>
2024-02-20 20:50:45 +0500

    react_testimonialApp added

---
+++ react_testimonialApp/src/Components/Card.jsx
@@ -0,0 +1,78 @@
+// import React from "react";
+// import {FaQuoteLeft, FaQuoteRight} from 'react-icons/fa';
+
+// const Card = (props) => {
+
+//     let review = props.review;
+
+//     return (
+//         <div className="flex flex-col md:relative">
+//             <div className="absolute top-[-7rem] z-[10] mx-auto ">
+//                 <img 
+//                 className="aspect-square rounded-full w-[140px] h-[140px] z-[25]"
+//                 src={review.image}></img>
+//                 <div className="w-[140px] h-[140px] bg-violet-500 rounded-full absolute top-[-6px] z-[-10] left-[10px]"></div>
+//             </div>
+            
+//             <div className="text-center mt-7">
+//                 <p className="tracking-wider
+//                  font-bold text-2xl capitalize">{review.name}</p>
+//                 <p className="text-violet-300 uppercase text-sm">{review.job}</p>
+//             </div>
+            
+//             <div className="text-violet-400 mx-auto mt-7">
+//                 <FaQuoteLeft/>
+//             </div>
+
+//             <div className="text-center mt-4 text-slate-500">
+//                 {review.text}
+//             </div>
+
+//             <div className="text-violet-400 mx-auto mt-7">
+//                 <FaQuoteRight/>
+//             </div>
+
+
+//         </div>
+//     );
+// }
+
+// export default Card;
+
+
+import React from "react";
+import {FaQuoteLeft, FaQuoteRight} from 'react-icons/fa';
+
+const Card = (props) => {
+    let review = props.review;
+    console.log(review);
+    return (
+        <div className="flex flex-col md:relative">
+
+            <div className="absolute top-[-7rem] z-[10] mx-auto ">
+                <img className="aspect-square rounded-full w-[140px] h-[140px] z-[25]"  src={review.image }></img>
+                <div className="w-[140px] h-[140px] bg-violet-500 rounded-full absolute top-[-6px] z-[-10] left-[10px]"></div>
+                
+            </div>
+            <div className="text-center mt-7">
+                <p className="tracking-wider font-bold text-2xl capitalize">{review.name}</p>
+                <p className="text-violet-300 uppercase text-sm">{review.job}</p>
+
+            </div>
+            <div className="text-violet-400 mx-auto mt-7">
+               <FaQuoteLeft/>
+             </div>
+
+             <div className="text-center mt-4 text-slate-500">
+                 {review.text}
+             </div>
+
+             <div className="text-violet-400 mx-auto mt-7">
+                 <FaQuoteRight/>
+             </div>
+
+        </div>
+    );
+}
+
+export default Card;
